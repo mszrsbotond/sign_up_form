40b66d5ea2b762ceb7fbff4b073b395f28ebcb3e
mszrsbotond <botond00723@gmail.com>
2025-05-24 14:58:33 +0200

    finished project

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let showPass = document.getElementById("show-password")
-let password = document.getElementById("password")
+const inputPass = document.querySelector(".show-password #show-password")
+const labelPass = document.querySelector(".show-password label")
+const password = document.querySelector("#password")
 
-showPass.addEventListener("click", () => {
+inputPass.addEventListener("click", () => {
     if(password.type == "password"){
-        password.type = "text"
+        password.type = "text";
+        labelPass.style.color = "var(--underline-blue)"
     }
     else{
         password.type = "password"
+        labelPass.style.color = "var(--gray)"
     }
 })
 
