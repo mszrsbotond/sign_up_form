d88573ed716e9a42b8abcc26ac3ffcdaa7054e28
mszrsbotond <botond00723@gmail.com>
2025-05-25 14:09:27 +0200

    finish styling submit button and project

--- script.js
+++ script.js
@@ -3,13 +3,30 @@ const labelPass = document.querySelector(".show-password label")
 const password = document.querySelector("#password")
 
 inputPass.addEventListener("click", () => {
-    if(password.type == "password"){
+    if (password.type == "password") {
         password.type = "text";
         labelPass.style.color = "var(--underline-blue)"
     }
-    else{
+    else {
         password.type = "password"
         labelPass.style.color = "var(--gray)"
     }
 })
 
+const submit = document.querySelector(".submit")
+
+const formBuiltIn = document.querySelector("form")
+
+const formElements = document.querySelectorAll(".form")
+formElements.forEach(formElement => {
+    formElement.addEventListener("input", () => {
+        if (formBuiltIn.checkValidity()) {
+            submit.style["background-color"] = "var(--underline-blue)"
+        }
+        else{
+            submit.style["background-color"] = "rgba(201, 201, 201, 0.1)"
+        }
+    })
+})
+
+console.log(formElements)
